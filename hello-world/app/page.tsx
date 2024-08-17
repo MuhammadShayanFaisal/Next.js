1a22c561a2d2f58a726a55dc5663d9199225393a
Muhammad Shayan Faisal <muhammadshayanfaisal@gmail.com>
2024-08-17 19:22:35 +0500

    Next.js

--- hello-world/app/page.tsx
+++ hello-world/app/page.tsx
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
-        <div>Hello World</div>
+        <div className="bg-black">
+          <h1 className="text-3xl text-orange-500 text-center">Hello World</h1>
+          <h2 className="text-2xl text-green-400 text-center">My name is Muhammad Shayan Faisal</h2>
+        </div>
         
   );
 }
